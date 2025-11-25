32f7c578ab5987ffed0403eb91ae730dc0edc02f
anthonyusuf <144717559+anthonyusuf@users.noreply.github.com>
2025-11-25 10:16:16 -0500

    Update CORS origin and add DB port
    Added support for database port configuration.

--- server.js
+++ server.js
@@ -16,19 +16,18 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",                     // dev
-      
+    "https://wotfrontend.onrender.com",
   ],
   methods: ["POST", "GET", "PUT"],
   credentials: true
 }));
 
-// PlanetScale MySQL Connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   ssl: { rejectUnauthorized: true }
 });
 
